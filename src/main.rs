a7802ad9a090794adf9289d2ea5012c7fb3412a4
unava1lable <910654119@qq.com>
2022-05-09 19:55:19 +0800

    add basic io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        let b = b.unwrap() as char;
+        println!("{}", b);
+    }
 }
