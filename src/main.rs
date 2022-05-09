626605363fa86582cebf2b2bd10c56cf40154137
unava1lable <910654119@qq.com>
2022-05-09 20:00:41 +0800

    modify interrupt

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use termion::raw::IntoRawMode;
 fn main() {
     let _stdout = io::stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let b = b.unwrap() as char;
-        println!("{}", b);
-        if b == 'q' {break;}
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?}\r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        if c == 'q' {break;}
     }
 }
