062dd9ab7ac48f607e7553afecbcf4d7a69bdbca
unava1lable <910654119@qq.com>
2022-05-09 20:07:44 +0800

    modify main loop

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use termion::raw::IntoRawMode;
 fn main() {
     let _stdout = io::stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?}\r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?}\r", b);
+                } else {
+                    println!("{:?} ({})", b, c);
+                }
+                if b == to_ctrl_byte('q') {break;}
+            }
+            Err(e) => die(e),
         }
-        if b == to_ctrl_byte('q') {break;}
     }
 }
 
