833d517c2c9f9b1480956208300217337b9e2875
unava1lable <910654119@qq.com>
2022-05-09 19:58:09 +0800

    raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = io::stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes() {
         let b = b.unwrap() as char;
         println!("{}", b);
