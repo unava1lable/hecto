c2cf63c16a5f86a06b2c45c85c11e23308927db2
unava1lable <910654119@qq.com>
2022-05-10 11:58:47 +0800

    modify main loop

--- src/editor.rs
+++ src/editor.rs
@@ -6,27 +6,37 @@ use termion::input::TermRead;
 pub struct Editor {}
 
 impl Editor {
+	pub fn default() -> Self {
+		Self {}
+	}
+
 	pub fn run(&self) {
 		let _stdout = io::stdout().into_raw_mode().unwrap();
-		for key in io::stdin().keys() {
-			match key {
-				Ok(key) => match key {
-					Key::Char(c) => {
-						if c.is_control() {
-							println!("{:?}\r", c as u8);
-						} else {
-							println!("{:?} ({})\r", c as u8, c);
-						}
-					}
-					Key::Ctrl('q') => break,
-					_ => println!("{:?}\r", key),
-				},
-				Err(e) => die(e),
+		loop {
+			if let Err(e) = self.process_keypress() {
+				die(&e);
 			}
+		}
+	}
+
+	fn process_keypress(&self) -> Result<(), io::Error> {
+		let pressed_key = read_key()?;
+		match pressed_key {
+			Key::Ctrl('q') => panic!("End"),
+			_ => ()
+		}
+		Ok(())
+	}
+}
+
+fn read_key() -> Result<Key, io::Error> {
+	loop {
+		if let Some(key) = io::stdin().lock().keys().next() {
+			return key;
 		}
 	}
 }
 
-fn die(e: io::Error) {
+fn die(e: &io::Error) {
 	panic!("{}", e)
 }
