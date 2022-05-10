e69c68100c7326cb89b760ff0913bf42b91f3a36
unava1lable <910654119@qq.com>
2022-05-10 11:49:17 +0800

    seperate code into editor.rs

---
+++ src/editor.rs
@@ -0,0 +1,32 @@
+use std::io;
+use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+
+pub struct Editor {}
+
+impl Editor {
+	pub fn run(&self) {
+		let _stdout = io::stdout().into_raw_mode().unwrap();
+		for key in io::stdin().keys() {
+			match key {
+				Ok(key) => match key {
+					Key::Char(c) => {
+						if c.is_control() {
+							println!("{:?}\r", c as u8);
+						} else {
+							println!("{:?} ({})\r", c as u8, c);
+						}
+					}
+					Key::Ctrl('q') => break,
+					_ => println!("{:?}\r", key),
+				},
+				Err(e) => die(e),
+			}
+		}
+	}
+}
+
+fn die(e: io::Error) {
+	panic!("{}", e)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,7 @@
-use std::io;
-use termion::raw::IntoRawMode;
-use termion::event::Key;
-use termion::input::TermRead;
+mod editor;
 
+use editor::Editor;
 fn main() {
-    let _stdout = io::stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(e) => die(e),
-        }
-    }
-}
-
-fn die(e: io::Error) {
-    panic!("{}", e);
+	let editor = Editor{};
+	editor.run();
 }
