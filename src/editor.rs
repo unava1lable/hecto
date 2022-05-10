20b283fb6204e56564156f5357ee220a4b019334
unava1lable <910654119@qq.com>
2022-05-10 12:01:44 +0800

    add quit statement

--- src/editor.rs
+++ src/editor.rs
@@ -3,26 +3,33 @@ use termion::raw::IntoRawMode;
 use termion::event::Key;
 use termion::input::TermRead;
 
-pub struct Editor {}
+pub struct Editor {
+	shoult_quit: bool
+}
 
 impl Editor {
 	pub fn default() -> Self {
-		Self {}
+		Self {
+			shoult_quit: false
+		}
 	}
 
-	pub fn run(&self) {
+	pub fn run(&mut self) {
 		let _stdout = io::stdout().into_raw_mode().unwrap();
 		loop {
 			if let Err(e) = self.process_keypress() {
 				die(&e);
 			}
+			if self.shoult_quit {
+				break;
+			}
 		}
 	}
 
-	fn process_keypress(&self) -> Result<(), io::Error> {
+	fn process_keypress(&mut self) -> Result<(), io::Error> {
 		let pressed_key = read_key()?;
 		match pressed_key {
-			Key::Ctrl('q') => panic!("End"),
+			Key::Ctrl('q') => self.shoult_quit = true,
 			_ => ()
 		}
 		Ok(())
