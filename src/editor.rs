4519b0b563bf02fbc2ff5364ce07b8e15a66c3ce
unava1lable <910654119@qq.com>
2022-05-12 15:00:54 +0800

    add status message

--- src/editor.rs
+++ src/editor.rs
@@ -2,8 +2,13 @@ use crate::Document;
 use crate::Terminal;
 use crate::Row;
 use std::env;
+use std::time::Duration;
+use std::time::Instant;
 use termion::event::Key;
+use termion::color;
 
+const STATUS_FG_COLOR: color::Rgb = color::Rgb(63, 63, 63);
+const STATUS_BG_COLOR: color::Rgb = color::Rgb(239, 239, 239);
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Default)]
@@ -12,20 +17,42 @@ pub struct Position {
 	pub y: usize,
 }
 
+struct StatusMessage {
+	text: String,
+	time: Instant,
+}
+
+impl StatusMessage {
+	fn from(message: String) -> Self {
+		Self {
+			time: Instant::now(),
+			text: message,
+		}
+	}
+}
+
 pub struct Editor {
 	shoult_quit: bool,
 	terminal: Terminal,
 	cursor_position: Position,
 	offset: Position,
 	document: Document,
+	status_message: StatusMessage,
 }
 
 impl Editor {
 	pub fn default() -> Self {
 		let args: Vec<String> = env::args().collect();
+		let mut initial_status = String::from("HELP: Ctrl-Q = quit");
 		let document = if args.len() > 1 {
 			let file_name = &args[1];
-			Document::open(&file_name).unwrap_or_default()
+			let doc = Document::open(&file_name);
+			if doc.is_ok() {
+				doc.unwrap()
+			} else {
+				initial_status = format!("Err: Could not open file: {}", file_name);
+				Document::default()
+			}
 		} else {
 			Document::default()
 		};
@@ -35,6 +62,7 @@ impl Editor {
 			cursor_position: Position::default(),
 			document,
 			offset: Position::default(),
+			status_message: StatusMessage::from(initial_status),
 		}
 	}
 
@@ -78,6 +106,8 @@ impl Editor {
 			println!("Goodbye!\r");
 		} else {
 			self.draw_rows();
+			self.draw_status_bar();
+			self.draw_message_bar();
 			Terminal::cursor_position(&Position {
 				x: self.cursor_position.x.saturating_sub(self.offset.x),
 				y: self.cursor_position.y.saturating_sub(self.offset.y),
@@ -179,7 +209,7 @@ impl Editor {
 
 	fn draw_rows(&self) {
 		let height = self.terminal.size().height;
-		for terminal_row in 0..height-1 {
+		for terminal_row in 0..height {
 			Terminal::clear_current_line();
 			if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
 				self.draw_row(row);
@@ -188,6 +218,43 @@ impl Editor {
 			} else {
 				println!("~\r");
 			}
+		}
+	}
+
+	fn draw_status_bar(&self) {
+		let mut status;
+		let width = self.terminal.size().width as usize;
+		let mut file_name = "[No Name]".to_string();
+		if let Some(name) = &self.document.file_name {
+			file_name = name.clone();
+			file_name.truncate(20);
+		}
+		status = format!("{} - {} lines", file_name, self.document.len());
+		let line_indicator = format!(
+			"{}/{}",
+			self.cursor_position.y.saturating_add(1),
+			self.document.len()
+		);
+		let len = status.len() + line_indicator.len();
+		if width > len {
+			status.push_str(&" ".repeat(width - status.len()));
+		}
+		status = format!("{}{}", status, line_indicator);
+		status.truncate(width);
+		Terminal::set_bg_color(STATUS_BG_COLOR);
+		Terminal::set_fg_color(STATUS_FG_COLOR);
+		println!("{}\r", status);
+		Terminal::reset_fg_color();
+		Terminal::reset_bg_color();
+	}
+
+	fn draw_message_bar(&self) {
+		Terminal::clear_current_line();
+		let message = &self.status_message;
+		if Instant::now() - message.time < Duration::new(5, 0) {
+			let mut text = message.text.clone();
+			text.truncate(self.terminal.size().width as usize);
+			print!("{}", text);
 		}
 	}
 }
