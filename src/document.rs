cdf00e751538550d0c255f7b7e2bb9c4c6730adb
unava1lable <910654119@qq.com>
2022-05-15 16:43:50 +0800

    add modified indicator

--- src/document.rs
+++ src/document.rs
@@ -8,6 +8,7 @@ use std::io::Write;
 pub struct Document {
 	rows: Vec<Row>,
 	pub file_name: Option<String>,
+	dirty: bool,
 }
 
 impl Document {
@@ -17,7 +18,11 @@ impl Document {
 		for value in contents.lines() {
 			rows.push(Row::from(value));
 		}
-		Ok(Self {rows, file_name: Some(filename.to_string())})
+		Ok(Self {
+			rows, 
+			file_name: Some(filename.to_string()),
+			dirty: false,
+		})
 	}
 
 	pub fn row(&self, index: usize) -> Option<&Row> {
@@ -33,9 +38,6 @@ impl Document {
 	}
 
 	fn insert_newline(&mut self, at: &Position) {
-		if at.y > self.len() {
-			return;
-		}
 		if at.y == self.len() {
 			self.rows.push(Row::default());
 			return;
@@ -45,6 +47,10 @@ impl Document {
 	}
 
 	pub fn insert(&mut self, at: &Position, c: char) {
+		if at.y > self.len() {
+			return;
+		}
+		self.dirty = true;
 		if c == '\n' {
 			self.insert_newline(at);
 			return;
@@ -54,7 +60,7 @@ impl Document {
 			let mut row = Row::default();
 			row.insert(0, c);
 			self.rows.push(row);
-		} else if at.y < self.len() {
+		} else {
 			let row = self.rows.get_mut(at.y).unwrap();
 			row.insert(at.x, c);
 		}
@@ -65,6 +71,7 @@ impl Document {
 		if at.y >= len {
 			return;
 		}
+		self.dirty = true;
 		if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
 			let next_row = self.rows.remove(at.y+1);
 			let row = self.rows.get_mut(at.y).unwrap();
@@ -75,14 +82,19 @@ impl Document {
 		}
 	}
 
-	pub fn save(&self) -> Result<(), io::Error> {
+	pub fn save(&mut self) -> Result<(), io::Error> {
 		if let Some(file_name) = &self.file_name {
 			let mut file = fs::File::create(file_name)?;
 			for row in &self.rows {
 				file.write_all(row.as_bytes())?;
 				file.write_all(b"\n")?;
 			}
+			self.dirty = false;
 		}
 		Ok(())
+	}
+
+	pub fn is_dirty(&self) -> bool {
+		self.dirty
 	}
 }
